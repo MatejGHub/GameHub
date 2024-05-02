af03fc31f5afff88e1780d996d3e71fc1daf0c84
Matej Stremfelj <matej.stremfelj26@gmail.com>
2024-05-02 17:41:35 +0200

    1. Building NAV

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+("536fe7b4b41d40688b68b6b9144c55db APIKEY");
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`
+        }}
+      >
+        <GridItem area={"nav"}>
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="pink.300" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
