09ff2c827c480fd360ce17c2c821035dc04633dd
Matej Stremfelj <matej.stremfelj26@gmail.com>
2024-05-17 17:03:32 +0200

    Restarting the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,5 @@
-import "./App.css";
-import { useState } from "react";
-import { Grid, GridItem } from "@chakra-ui/react";
-import { Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenresList";
-import { Genres } from "./hooks/useGenres";
-
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genres | null>(null);
-
-  console.log(selectedGenre);
-
-  return (
-    <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`
-        }}
-        templateColumns={{ lg: "18% 82%" }}
-      >
-        <GridItem area={"nav"}>
-          <NavBar />
-        </GridItem>
-        <Show above="lg">
-          <GridItem area={"aside"} paddingX={5} w={"230px"}>
-            <GenreList
-              onSelectGenre={(genre) => {
-                setSelectedGenre(genre);
-              }}
-            />
-          </GridItem>
-        </Show>
-        <GridItem area={"main"}>
-          <GameGrid selectedGenre={selectedGenre} />
-        </GridItem>
-      </Grid>
-    </>
-  );
+  return <></>;
 }
-
-// 1. Take care of game card skeletons - width problem
 
 export default App;
