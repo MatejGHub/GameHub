6266341854c50278aef10a55d47079e5f40fbef3
Matej Stremfelj <matej.stremfelj26@gmail.com>
2024-05-21 13:18:46 +0200

    Handle games with no image

--- src/services/image-url.tsx
+++ src/services/image-url.tsx
@@ -1,5 +1,7 @@
+import noImage from "../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media/".length;
   const prefix = url.slice(0, index);
   const suffix = url.slice(index);
